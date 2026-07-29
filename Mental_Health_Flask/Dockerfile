9940501b10423ac62de72fa27da3f7da1dfa4394
Nagalakshmi5739 <nagalakshmi5739@gmail.com>
2026-07-29 16:32:50 +0530

    Aggressive memory optimizations for Render 512MB limit

--- Mental_Health_Flask/Dockerfile
+++ Mental_Health_Flask/Dockerfile
@@ -18,4 +18,5 @@ ENV PORT=7860
 ENV TF_CPP_MIN_LOG_LEVEL=2
 ENV TF_ENABLE_ONEDNN_OPTS=0
 
-CMD ["gunicorn", "app:app", "--bind", "0.0.0.0:7860", "--workers", "1", "--threads", "2", "--timeout", "120", "--log-file", "-"]
+CMD ["gunicorn", "app:app", "--bind", "0.0.0.0:7860", "--workers", "1", "--threads", "1", "--timeout", "120", "--log-file", "-"]
+
--- Mental_Health_Flask/app.py
+++ Mental_Health_Flask/app.py
@@ -7,7 +7,9 @@ import pandas as pd
 
 os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "3")
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
-os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["TF_NUM_INTEROP_THREADS"] = "1"
+os.environ["TF_NUM_INTRAOP_THREADS"] = "1"
+os.environ["MALLOC_TRIM_THRESHOLD_"] = "65536"
 warnings.filterwarnings("ignore", category=UserWarning, module="google.protobuf")
 
 from flask import Flask, render_template, request, jsonify, send_file
